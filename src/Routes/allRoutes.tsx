a35810db4b3bea676c36779467778942f3c0b100
qraxiss <qraxiss@gmail.com>
2024-02-17 11:28:45 +0300

    unneccesary routes deleted

--- src/Routes/allRoutes.tsx
+++ src/Routes/allRoutes.tsx
@@ -1,9 +1,7 @@
 import { Navigate } from 'react-router-dom'
 
-//dashbord
 import Home from 'Pages/Home'
-//Catalog
-//shop
+
 import ShopIndex from 'Pages/Shop'
 import Trackorder from 'Pages/Shop/Trackorder'
 import PaymentIndex from 'Pages/Shop/Indexpayment'
@@ -24,40 +22,15 @@ import Signin from 'Pages/User/SignIn'
 import Passwordreset from 'Pages/User/Passwordreset'
 import Passwordcreate from 'Pages/User/Passwordcreate'
 import Successmsg from 'Pages/User/Successmsg'
-import Verifyemail from 'Pages/User/Verifyemail'
 import Logout from 'Pages/User/Logout'
 import Error404 from 'Pages/User/Error404'
 import Error500 from 'Pages/User/Error500'
-
-//pages / Email
-import Blackfriday from 'Pages/Product/Email Product/Blackfriday'
-import Flashsale from 'Pages/Product/Email Product/Flashsale'
-import Ordersuccess from 'Pages/Product/Email Product/Ordersuccess'
-import Ordersuccess2 from 'Pages/Product/Email Product/Ordersuccess2'
 
 //Productdetails
 import Productdetails from 'Pages/Product/Productdetails'
 
 //about
 import About from 'Pages/Product/About'
-
-//Purchase Guide
-import Purchaseguide from 'Pages/Product/Purchaseguide'
-
-//Terms of Conditions
-import Termsconditions from 'Pages/Product/Termsconditions'
-
-//Privacy Policy
-import Privacypolicy from 'Pages/Product/Privacypolicy'
-
-//Storelocator
-import Storelocator from 'Pages/Product/Storelocator'
-
-//FAQ
-import FAQ from 'Pages/Product/FAQ'
-
-//Invoice
-import Invoice from 'Pages/Product/Invoice'
 
 //contact
 import ContactUs from 'Pages/ContactUs/Contact'
@@ -84,21 +57,8 @@ const authProtectedRoutes = [
     //My Account
     { path: '/account', component: <MyAccount /> },
 
-    //about
     { path: '/about-us', component: <About /> },
-    //Purchase Guide
-    { path: '/purchase-guide', component: <Purchaseguide /> },
-    //Terms of Conditions
-    { path: '/terms-conditions', component: <Termsconditions /> },
-    //Privacy Policy
-    { path: '/privacy-policy', component: <Privacypolicy /> },
-    //Storelocator
-    { path: '/store-locator', component: <Storelocator /> },
-    //FAQ
-    { path: '/ecommerce-faq', component: <FAQ /> },
-    //Invoice
-    { path: '/invoice', component: <Invoice /> },
-    //contact us
+
     { path: '/contact', component: <ContactUs /> },
 
     { path: '*', component: <Navigate to="/" /> }
