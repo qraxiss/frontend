fd309fd1d5f935cb184d54382d61c7246fec981d
qraxiss <qraxiss@gmail.com>
2024-02-24 11:57:52 +0300

    Update Cardshop.tsx

--- src/Pages/Shop/Cardshop.tsx
+++ src/Pages/Shop/Cardshop.tsx
@@ -38,7 +38,7 @@ const Cardshop = () => {
         setProductcount(productData?.filter((delet: any) => delet.id !== id))
     }
 
-    const assinged = (productcount || [])?.map((M) => M.Total)
+    const assinged = (cart.data || [])?.map((M:any) => M.Total)
     let subtotal = 0
     for (let i = 0; i < assinged.length; i++) {
         subtotal += Math.round(assinged[i])
