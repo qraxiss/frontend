e20e16943e44674bafa3645f80dd5e7b966cbcd6
qraxiss <qraxiss@gmail.com>
2024-02-24 22:24:46 +0300

    searchParams tracked successfully

--- src/Components/Index.tsx
+++ src/Components/Index.tsx
@@ -5,14 +5,28 @@ import Filters from 'Pages/Catalog/Filters'
 import { useQuery } from 'lib/query-wrapper'
 
 import { getProductsByCategorySlug } from 'lib/common-queries'
-
-import { useParams } from 'react-router-dom'
+import { useParams,useSearchParams } from 'react-router-dom'
+import { params } from 'lib/getQueryVariables'
 
 const Index = ({ name, cxxl, clg, cmd, cxl }: any) => {
   let { child } = useParams()
+
+  let [searchParams, setSearchParams] = useSearchParams()
+  let { start } = params(searchParams)
+
+  useEffect(()=>{
+    if (!start){
+      setSearchParams({
+        start: "0"
+      })
+    }
+  }, [child])
+
+
   const products = useQuery(getProductsByCategorySlug, {
     variables: {
-      slug: child
+      slug: child,
+      start: Number(start)
     }
   })
 
@@ -24,11 +38,11 @@ const Index = ({ name, cxxl, clg, cmd, cxl }: any) => {
       setFilterOptions(products.data.variants)
       setFilterlist(products.data.products)
     }
-  }, [products.loading, child])
+  }, [products.loading, child, start])
 
   return (
     <React.Fragment>
-      <Filters setFilterlist={setFilterlist} filterList={filterList} name={name} filterOptions={filterOptions} />
+      <Filters setFilterlist={setFilterlist} filterList={filterList} name={name} filterOptions={filterOptions} setSearchParams={setSearchParams} />
       <CatalogCollection filterList={filterList} cxxl={cxxl} clg={clg} cmd={cmd} cxl={cxl} />
     </React.Fragment>
   )
--- src/Pages/Catalog/CatalogCollection.tsx
+++ src/Pages/Catalog/CatalogCollection.tsx
@@ -8,7 +8,7 @@ import { useQuery, useMutation } from 'lib/query-wrapper'
 import { addItemToCart, cartQuery } from 'lib/common-queries'
 import config from 'config/config'
 
-const CatalogCollection = ({ cxxl, cxl, clg, cmd, cheight, filterList }: any) => {
+const CatalogCollection = ({ cxxl, cxl, clg, cmd, cheight, filterList, setSearchParams }: any) => {
   console.log(filterList)
 
   const addItem = useMutation(addItemToCart)
