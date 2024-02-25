07098d25b66ceedc3b8a9e5ea7adbeba3b16911b
Mehmet Alperen Yedik <121613095+Alperen7K@users.noreply.github.com>
2024-02-25 12:51:07 +0300

    type decleration for products and productSettings

---
+++ src/models/ProductType.ts
@@ -0,0 +1,13 @@
+export type filterSettingsType = { name: string; options: { value: string; id: number }[]; choosen: string[]; open: boolean }
+
+export type productListType = {
+  price: number
+  slug: string
+  images: { url: string }[]
+  name: string
+  variants: productVariant[]
+}
+export type productVariant = {
+  options: { value: string }[]
+  name: string
+}
