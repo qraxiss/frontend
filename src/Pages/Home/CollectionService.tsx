102e3b04add1f70a48213a2968c53b8ce779659b
Mehmet Alperen Yedik <121613095+Alperen7K@users.noreply.github.com>
2024-02-25 20:49:05 +0300

    Translate

--- src/Pages/Home/CollectionService.tsx
+++ src/Pages/Home/CollectionService.tsx
@@ -4,8 +4,7 @@ import { Container, Row, Col, Image } from 'react-bootstrap'
 import { CommonService } from 'Components/CommonService'
 
 //img
-import featuresimg3 from 'assets/images/ecommerce/features/img-3.jpg'
-import featuresimg1 from 'assets/images/ecommerce/features/img-1.jpg'
+import img1 from 'assets/images/Carusel/slide2.png'
 import { CommonTitle } from 'Components/Homepage'
 
 const Service = () => {
@@ -17,25 +16,26 @@ const Service = () => {
           <Row>
             <Col lg={6}>
               <Link to="#" className="product-banner-1 mt-4 mt-lg-0 rounded overflow-hidden position-relative d-block">
-                <Image src={featuresimg3} fluid rounded alt="" />
+                <Image src={img1} fluid rounded alt="" />
                 <div className="bg-overlay blue"></div>
                 <div className="product-content p-4">
-                  <p className="text-uppercase text-white mb-2">Up to 50-70%</p>
-                  <h1 className="text-white lh-base fw-medium ff-secondary"> Women's Sportwere Sales</h1>
+                  <p className=" text-white mb-2">Yüzde 50'ye varan indirimler</p>
+                  <h1 className="text-white lh-base fw-medium "> Hidrolik Ürünleri</h1>
                   <div className="product-btn mt-4 text-white">
-                    Shop Now <i className="bi bi-arrow-right ms-2"></i>
+                    Alışveriş Yap <i className="bi bi-arrow-right ms-2"></i>
                   </div>
                 </div>
               </Link>
             </Col>
             <Col lg={6}>
               <Link to="#" className="product-banner-1 mt-4 mt-lg-0 rounded overflow-hidden position-relative d-block">
-                <Image src={featuresimg1} fluid rounded alt="" />
+                <Image src={img1} fluid rounded alt="" />
+                <div className="bg-overlay blue"></div>
                 <div className="product-content p-4">
-                  <p className="text-uppercase fw-medium text-secondary mb-2">Summer Sales</p>
-                  <h1 className="lh-base ff-secondary text-dark">Trendy Fashion Clothes</h1>
-                  <div className="product-btn mt-4">
-                    Shop Now <i className="bi bi-arrow-right ms-2"></i>
+                  <p className=" text-white mb-2">Yüzde 50'ye varan indirimler</p>
+                  <h1 className="text-white lh-base fw-medium "> Rakor Ürünleri</h1>
+                  <div className="product-btn mt-4 text-white">
+                    Alışveriş Yap <i className="bi bi-arrow-right ms-2"></i>
                   </div>
                 </div>
               </Link>
@@ -45,7 +45,7 @@ const Service = () => {
       </section>
       <Container>
         <CommonTitle
-          title="Top Picks Products Of The Week"
+          title="HAFTANIN EN İYİ ÜRÜNLERİ"
           dicription="This ranges from women and men's outfits to children's clothing, shoes, accessories, and more. People love their clothes, and fashion isn't going anywhere!"
         />
       </Container>
