0fbe5271a6f6719c035c40eb0ca473b73133d3af
qraxiss <qraxiss@gmail.com>
2024-02-26 14:10:23 +0300

    Update Section.tsx

--- src/Pages/Home/Section.tsx
+++ src/Pages/Home/Section.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Container, Row, Col, Image } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 
@@ -35,42 +35,61 @@ const query = gql`
 `
 
 export default function Section() {
-  const { data, loading } = useQuery(query)
+  const { data, loading, error } = useQuery(query)
 
-  if (loading) {
-    return
-  }
+  const [campaigns, setCampaigns] = useState<any[]>([])
+
+  useEffect(()=>{
+    if (loading) {
+      return
+    }
+    
+    if (error) {
+      return
+    }
+
+    if (!data){
+      return
+    }
+
+    data.campaigns.forEach((item:any)=>{
+      item.image.url = config.serverUrl + item.image.url
+    })
+
+    setCampaigns(data.campaigns)
+
+  }, [loading])
 
   return (
     <section className="section pb-0 mt-4">
       <Container fluid>
         <Row className="g-2">
           <Col lg={7}>
-            <Link to={data.campaigns[0].btnLink} className="product-banner-1 mt-4 mt-lg-0 rounded overflow-hidden d-block">
+            <Link to={campaigns[0]?.btnLink} className="product-banner-1 mt-4 mt-lg-0 rounded overflow-hidden d-block">
               <Image
-                src={config.serverUrl + data.campaigns[0].image.url}
+                src={campaigns[0]?.image?.url}
                 className="w-100 object-fit-cover"
                 rounded
                 alt=""
                 style={{ maxHeight: '480px' }}
               />
-              <div className="product-content p-3 ps-5" style={{ maxWidth: '390px' }}>
-                <p className="text-uppercase fs-15 text-secondary fw-semibold mb-2">{data.campaigns[0].heading}</p>
-                <h1 className="display-5 lh-base text-dark ff-secondary">{data.campaigns[0].subHeading}</h1>
+              <div className="product-content p-3 ps-5" >
+                <p className="text-uppercase fs-15 text-secondary fw-semibold mb-2">{campaigns[0]?.heading}</p>
+                <h1 className="display-5 lh-base text-dark ff-secondary">{campaigns[0]?.subHeading}</h1>
                 <div className="product-btn mt-4">
-                  {data.campaigns[0].btnText} <i className="bi bi-arrow-right ms-2"></i>
+                  {campaigns[0]?.btnText} <i className="bi bi-arrow-right ms-2"></i>
                 </div>
               </div>
             </Link>
           </Col>
-          <Col lg={5}>
+          <Col lg={3}>
             <Row className="g-2">
-              {data.campaigns.slice(1, data.campaigns.length).map((campaign: any) => {
+              {campaigns.slice(1, campaigns.length).map((campaign: any) => {
                 return (
                   <Col key={campaign.image.url} lg={12}>
                     <Link to={campaign.btnLink} className="product-banner-1 mt-4 mt-lg-0 rounded overflow-hidden d-block">
                       <Image
-                        src={config.serverUrl + campaign.image.url}
+                        src={campaign.image.url}
                         className="w-100 object-fit-cover"
                         alt=""
                         style={{ maxHeight: '236px' }}
