d8ab22ecf564ca6c5764382c92cbc9c872166a4b
qraxiss <qraxiss@gmail.com>
2024-02-26 11:23:27 +0300

    header ağır update

--- src/Layout/Header.tsx
+++ src/Layout/Header.tsx
@@ -2,9 +2,6 @@ import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { Container, Dropdown, Button, Row, Col, Card, Image, Navbar, Nav } from 'react-bootstrap'
 
-//img
-import avtar1 from 'assets/images/users/avatar-1.jpg'
-
 import { CardModal, SearchModal } from 'Components/MainModal'
 import { withTranslation } from 'react-i18next'
 import withRouter from 'Components/withRouter'
@@ -45,13 +42,57 @@ const query = gql`
         }
       }
     }
+
     profilePicture {
       url
     }
+
+    icon {
+      data {
+        attributes {
+          account {
+            data {
+              attributes {
+                url
+              }
+            }
+          }
+          cart {
+            data {
+              attributes {
+                url
+              }
+            }
+          }
+          wishlist {
+            data {
+              attributes {
+                url
+              }
+            }
+          }
+        }
+      }
+    }
   }
 `
-function ShoppingIcon(props: { handlecardShow: any }) {
-  let cartData = useQuery(cartQuery)
+function ShoppingIcon(props: { handlecardShow: any; iconPath: string }) {
+  let { data, loading, error } = useQuery(cartQuery)
+
+  const [cartCount, setCartCount] = useState(0)
+  const [totalPrice, setTotalPrice] = useState(0)
+  useEffect(() => {
+    if (!!data && !loading && !error) {
+      setCartCount(data.length)
+
+      let subtotal = 0
+      for (let i = 0; i < data.length; i++) {
+        subtotal += data[i].product.price * data[i].count
+      }
+      setTotalPrice(subtotal)
+    }
+  }, [loading])
+
   return (
     <div className="topbar-head-dropdown ms-1 header-item">
       <Button
@@ -62,22 +103,215 @@ function ShoppingIcon(props: { handlecardShow: any }) {
         aria-controls="ecommerceCart"
         onClick={props.handlecardShow}
       >
-        <i className="ph-shopping-cart fs-18"></i>
-        <span className="position-absolute topbar-badge cartitem-badge fs-10 translate-middle badge rounded-pill bg-danger">
-          {!cartData.loading ? cartData?.data.length : 0}
-        </span>
+        {/* <i className="ph-shopping-cart fs-18"></i> */}
+        <Image className="rounded-circle header-profile-user" src={props.iconPath} alt="Header Avatar" />
+        <span className="position-absolute topbar-badge cartitem-badge fs-10 translate-middle badge rounded-pill bg-danger">{cartCount}</span>
+      </Button>
+      ${totalPrice}
+    </div>
+  )
+}
+
+function WishListIcon(props: { handlecardShow: any; iconPath: string }) {
+  return (
+    <div className="topbar-head-dropdown ms-1 header-item">
+      <Button
+        type="button"
+        className="btn btn-icon btn-topbar btn-ghost-dark rounded-circle text-muted"
+        data-bs-toggle="offcanvas"
+        data-bs-target="#ecommerceCart"
+        aria-controls="ecommerceCart"
+        onClick={props.handlecardShow}
+      >
+        <Image className="rounded-circle header-profile-user" src={props.iconPath} alt="Header Avatar" />
+        <span className="position-absolute topbar-badge cartitem-badge fs-10 translate-middle badge rounded-pill bg-danger">0</span>
       </Button>
     </div>
   )
 }
 
-const Header = (props: any) => {
+function Account(props: { iconPath: string }) {
   let jwt = localStorage.getItem('jwt')
 
-  let { data, loading, error } = useQuery(query)
+  return (
+    <div className="dropdown header-item dropdown-hover-end">
+      <Dropdown>
+        <Dropdown.Toggle id="page-header-user-dropdown" bsPrefix="btn" className="btn btn-icon btn-topbar btn-link rounded-circle" as="a">
+          <Image className="rounded-circle header-profile-user" src={props.iconPath} alt="Header Avatar" />
+        </Dropdown.Toggle>
 
-  let logodark = !loading ? config.serverUrl + data.logo.text.url : ''
-  let logolight = !loading ? config.serverUrl + data.logo.text.url : ''
+        {jwt ? (
+          <Dropdown.Menu>
+            <Dropdown.Item>
+              <Link to="/shop/orders">
+                <i className="bi bi-truck text-muted fs-16 align-middle me-1"></i> <span className="align-middle">Track Orders</span>
+              </Link>
+            </Dropdown.Item>
+            <Dropdown.Item>
+              <Link to="/account">
+                <span className="badge bg-success-subtle text-success mt-1 float-end">New</span>
+                <i className="mdi mdi-cog-outline text-muted fs-16 align-middle me-1"></i> <span className="align-middle">Settings</span>
+              </Link>
+            </Dropdown.Item>
+            <Dropdown.Item>
+              <Link to="/logout">
+                <i className="bi bi-box-arrow-right text-muted fs-16 align-middle me-1"></i>{' '}
+                <span className="align-middle" data-key="t-logout">
+                  Logout
+                </span>
+              </Link>
+            </Dropdown.Item>
+          </Dropdown.Menu>
+        ) : (
+          <Dropdown.Menu>
+            <Dropdown.Item href="/signin">
+              <i className="bi bi-box-arrow-right text-muted fs-16 align-middle me-1"></i>{' '}
+              <span className="align-middle" data-key="t-logout">
+                Sign In
+              </span>
+            </Dropdown.Item>
+            <Dropdown.Item href="/signup">
+              <i className="bi bi-box-arrow-right text-muted fs-16 align-middle me-1"></i>{' '}
+              <span className="align-middle" data-key="t-logout">
+                Sign Up
+              </span>
+            </Dropdown.Item>
+          </Dropdown.Menu>
+        )}
+      </Dropdown>
+    </div>
+  )
+}
+
+function Pages(props: { categories: any; logo: any; menuShow: any; showMenu: any; t: any }) {
+  return (
+    <Navbar.Collapse id="navbarSupportedContent">
+      <Nav as="ul" className="mx-lg-auto mb-2 mb-lg-0" id="navigation-menu">
+        {/* LOGO */}
+        <li className="nav-item d-block d-lg-none">
+          <Link to="/" className="d-block p-3 h-auto text-center">
+            <Image src={props.logo.text.url} alt="" height="25" className="card-logo-dark mx-auto" />
+          </Link>
+          <Link to="/" className="d-block p-3 h-auto text-center">
+            <Image src={props.logo.text.url} alt="" height="25" className="card-logo-light mx-auto" />
+          </Link>
+        </li>
+        {props.categories.map((item: any) => {
+          return (
+            <li className="dropdown nav-item dropdown-hover" key={item.slug}>
+              <Link
+                className="dropdown-toggle nav-link"
+                data-key="t-home"
+                to="/#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+                onClick={(e) => {
+                  e.preventDefault()
+                  props.menuShow(item.name)
+                }}
+              >
+                {props.t(item.name)}
+              </Link>
+
+              <ul
+                className={
+                  props.showMenu === item.name
+                    ? 'dropdown-menu dropdown-menu-md dropdown-menu-center dropdown-menu-list submenu show'
+                    : 'dropdown-menu dropdown-menu-md dropdown-menu-center dropdown-menu-list submenu'
+                }
+              >
+                {item.childs.map((child: any) => {
+                  return (
+                    <li className="nav-item" key={`${item.slug}-${child.slug}`}>
+                      <Link to={`/category/${item.slug}/${child.slug}`} className="nav-link" data-key={child.slug}>
+                        {props.t(child.name)}
+                      </Link>
+                    </li>
+                  )
+                })}
+              </ul>
+            </li>
+          )
+        })}
+      </Nav>
+    </Navbar.Collapse>
+  )
+}
+
+function Search(props: { handleShow: any }) {
+  return (
+    <Button
+      type="button"
+      className="btn btn-icon btn-topbar btn-ghost-dark rounded-circle text-muted"
+      data-bs-toggle="modal"
+      data-bs-target="#searchModal"
+      onClick={props.handleShow}
+    >
+      <i className="bx bx-search fs-22"></i>
+    </Button>
+  )
+}
+
+function Logo(props: { logo: any }) {
+  return (
+    <Navbar.Brand className="d-none d-lg-block">
+      <Link to="/">
+        <div className="logo-dark">
+          <Image src={props.logo.text.url} alt="" height="50" />
+        </div>
+        <div className="logo-light">
+          <Image src={props.logo.text.url} alt="" height="50" />
+        </div>
+      </Link>
+    </Navbar.Brand>
+  )
+}
+
+function MoreButton(props: { handleShowColl: any }) {
+  return (
+    <Button className="btn btn-soft-primary btn-icon d-lg-none collapsed" aria-controls="navbarSupportedContent" onClick={props.handleShowColl}>
+      <i className="bi bi-list fs-20"></i>
+    </Button>
+  )
+}
+
+const Header = (props: any) => {
+  let { data, loading, error } = useQuery(query)
+  const [categories, setCategories] = useState([])
+  const [logo, setLogo] = useState({
+    text: {
+      url: ''
+    }
+  })
+  const [icon, setIcon] = useState({
+    account: {
+      url: ''
+    },
+    cart: {
+      url: ''
+    },
+    wishlist: {
+      url: ''
+    }
+  })
+  useEffect(() => {
+    if (!!data && !loading && !error) {
+      Object.keys(data.icon).map((key) => {
+        data.icon[key] = {
+          url: config.serverUrl + data.icon[key].url
+        }
+      })
+      setIcon(data.icon)
+
+      setCategories(data.parentCategories)
+      setLogo({
+        text: {
+          url: config.serverUrl + data.logo.text.url
+        }
+      })
+    }
+  }, [loading])
 
   const [show, setShow] = useState(false)
   const handleClose = () => setShow(false)
@@ -90,7 +324,6 @@ const Header = (props: any) => {
   const handlecardShow = () => setCard(true)
 
   const [showMenu, setShowMenu] = useState<any>('')
-
   const menuShow = (item: any) => {
     var windowSize = document.documentElement.clientWidth
     if (windowSize < 992 && showMenu !== item) {
@@ -99,6 +332,7 @@ const Header = (props: any) => {
       setShowMenu('')
     }
   }
+
 
   const path = props.router.location.pathname
   useEffect(() => {
@@ -155,7 +389,6 @@ const Header = (props: any) => {
       }
     }
   }
-
   window.addEventListener('scroll', function (ev) {
     ev.preventDefault()
     windowScroll()
@@ -174,7 +407,6 @@ const Header = (props: any) => {
     }
   }
 
-  // Resize Nav Collapse
   const windowResizeHover = () => {
     var windowSize = document.documentElement.clientWidth
     if (windowSize > 992) {
@@ -197,156 +429,17 @@ const Header = (props: any) => {
     <React.Fragment>
       <Navbar className="navbar-expand-lg ecommerce-navbar" id="navbar" expanded={false}>
         <Container>
-          <Navbar.Brand className="d-none d-lg-block">
-            <Link to="/">
-              <div className="logo-dark">
-                <Image src={logodark} alt="" height="50" />
-              </div>
-              <div className="logo-light">
-                <Image src={logolight} alt="" height="50" />
-              </div>
-            </Link>
-          </Navbar.Brand>
-          <Button className="btn btn-soft-primary btn-icon d-lg-none collapsed" aria-controls="navbarSupportedContent" onClick={handleShowColl}>
-            <i className="bi bi-list fs-20"></i>
-          </Button>
-
-          <Navbar.Collapse id="navbarSupportedContent">
-            <Nav as="ul" className="mx-lg-auto mb-2 mb-lg-0" id="navigation-menu">
-              {/* LOGO */}
-              <li className="nav-item d-block d-lg-none">
-                <Link to="/#" className="d-block p-3 h-auto text-center">
-                  <Image src={logodark} alt="" height="25" className="card-logo-dark mx-auto" />
-                </Link>
-                <Link to="/#" className="d-block p-3 h-auto text-center">
-                  <Image src={logolight} alt="" height="25" className="card-logo-light mx-auto" />
-                </Link>
-              </li>
-              {(!loading ? data.parentCategories : []).map((item: any) => {
-                return (
-                  <li className="dropdown nav-item dropdown-hover" key={item.slug}>
-                    <Link
-                      className="dropdown-toggle nav-link"
-                      data-key="t-home"
-                      to="/#"
-                      role="button"
-                      data-bs-toggle="dropdown"
-                      aria-expanded="false"
-                      onClick={(e) => {
-                        e.preventDefault()
-                        menuShow(item.name)
-                      }}
-                    >
-                      {props.t(item.name)}
-                    </Link>
-
-                    <ul
-                      className={
-                        showMenu === item.name
-                          ? 'dropdown-menu dropdown-menu-md dropdown-menu-center dropdown-menu-list submenu show'
-                          : 'dropdown-menu dropdown-menu-md dropdown-menu-center dropdown-menu-list submenu'
-                      }
-                    >
-                      {item.childs.map((child: any) => {
-                        return (
-                          <li className="nav-item" key={`${item.slug}-${child.slug}`}>
-                            <Link to={`/category/${item.slug}/${child.slug}`} className="nav-link" data-key={child.slug}>
-                              {props.t(child.name)}
-                            </Link>
-                          </li>
-                        )
-                      })}
-                    </ul>
-                  </li>
-                )
-              })}
-            </Nav>
-          </Navbar.Collapse>
+          <Logo logo={logo}></Logo>
+          <MoreButton handleShowColl={handleShowColl}></MoreButton>
+          <Pages categories={categories} logo={logo} t={props.t} menuShow={menuShow} showMenu={showMenu}></Pages>
 
           <div className="bg-overlay navbar-overlay" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent.show"></div>
           <div className="d-flex align-items-center">
-            {/* Search */}
-            <Button
-              type="button"
-              className="btn btn-icon btn-topbar btn-ghost-dark rounded-circle text-muted"
-              data-bs-toggle="modal"
-              data-bs-target="#searchModal"
-              onClick={handleShow}
-            >
-              <i className="bx bx-search fs-22"></i>
-            </Button>
+            <Search handleShow={handleShow}></Search>
             <SearchModal show={show} handleClose={handleClose} />
-            {!!jwt ? <ShoppingIcon handlecardShow={handlecardShow} /> : ''}
-            <Dropdown className="topbar-head-dropdown ms-2 header-item dropdown-hover-end" align="start">
-              <Dropdown.Toggle className="btn btn-icon btn-topbar btn-ghost-dark rounded-circle text-muted" bsPrefix="btn">
-                <i className="bi bi-sun align-middle fs-20"></i>
-              </Dropdown.Toggle>
-              <Dropdown.Menu className="dropdown-menu p-2 dropdown-menu-end" id="light-dark-mode">
-                <Dropdown.Item eventKey="light" onClick={() => props.handleMood('light')}>
-                  <i className="bi bi-sun align-middle me-2"></i> Default (light mode)
-                </Dropdown.Item>
-                <Dropdown.Item eventKey="dark" onClick={() => props.handleMood('dark')}>
-                  <i className="bi bi-moon align-middle me-2"></i> Dark
-                </Dropdown.Item>
-                <Dropdown.Item eventKey="light" onClick={() => props.handleMood('light')}>
-                  <i className="bi bi-moon-stars align-middle me-2"></i> Auto (system default)
-                </Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
-            {!!jwt ? (
-              <div className="dropdown header-item dropdown-hover-end">
-                <Dropdown>
-                  <Dropdown.Toggle id="page-header-user-dropdown" bsPrefix="btn" className="btn btn-icon btn-topbar btn-link rounded-circle" as="a">
-                    <Image className="rounded-circle header-profile-user" src={config.serverUrl + data?.profilePicture?.url} alt="Header Avatar" />
-                  </Dropdown.Toggle>
-
-                  <Dropdown.Menu>
-                    <Dropdown.Item>
-                      <Link to="/shop/orders">
-                        <i className="bi bi-truck text-muted fs-16 align-middle me-1"></i> <span className="align-middle">Track Orders</span>
-                      </Link>
-                    </Dropdown.Item>
-                    <Dropdown.Item>
-                      <Link to="/account">
-                        <span className="badge bg-success-subtle text-success mt-1 float-end">New</span>
-                        <i className="mdi mdi-cog-outline text-muted fs-16 align-middle me-1"></i> <span className="align-middle">Settings</span>
-                      </Link>
-                    </Dropdown.Item>
-                    <Dropdown.Item>
-                      <Link to="/logout">
-                        <i className="bi bi-box-arrow-right text-muted fs-16 align-middle me-1"></i>{' '}
-                        <span className="align-middle" data-key="t-logout">
-                          Logout
-                        </span>
-                      </Link>
-                    </Dropdown.Item>
-                  </Dropdown.Menu>
-                </Dropdown>
-              </div>
-            ) : (
-              <div className="dropdown header-item dropdown-hover-end">
-                <Dropdown>
-                  <Dropdown.Toggle id="page-header-user-dropdown" bsPrefix="btn" className="btn btn-icon btn-topbar btn-link rounded-circle" as="a">
-                    <Image className="rounded-circle header-profile-user" src={config.serverUrl + data?.profilePicture?.url} alt="Header Avatar" />
-                  </Dropdown.Toggle>
-
-                  <Dropdown.Menu>
-                    <Dropdown.Item href="/signin">
-                      <i className="bi bi-box-arrow-right text-muted fs-16 align-middle me-1"></i>{' '}
-                      <span className="align-middle" data-key="t-logout">
-                        Sign In
-                      </span>
-                    </Dropdown.Item>
-                    <Dropdown.Item href="/signup">
-                      <i className="bi bi-box-arrow-right text-muted fs-16 align-middle me-1"></i>{' '}
-                      <span className="align-middle" data-key="t-logout">
-                        Sign Up
-                      </span>
-                    </Dropdown.Item>
-                  </Dropdown.Menu>
-                </Dropdown>
-              </div>
-            )}
+            <Account iconPath={icon.account.url}></Account>
+            <WishListIcon iconPath={icon.wishlist.url} handlecardShow={handlecardShow}></WishListIcon>
+            <ShoppingIcon iconPath={icon.cart.url} handlecardShow={handlecardShow} />
           </div>
         </Container>
       </Navbar>
