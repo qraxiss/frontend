b2e187984b1b264c9625b7174361d19d1d70cf31
qraxiss <qraxiss@gmail.com>
2024-02-28 14:25:16 +0300

    if jwt changed renew

--- src/Components/context/user-context.tsx
+++ src/Components/context/user-context.tsx
@@ -13,5 +13,11 @@ export const useUser = () => {
 
 export const UserProvider = ({ children }: any) => {
     let [jwt, setJwt] = useState<string | undefined | null>(localStorage.getItem('jwt'))
+    let tempJwt = localStorage.getItem('jwt')
+
+    if (jwt !== tempJwt){
+        setJwt(tempJwt)
+    }
+
     return <UserContext.Provider value={{ jwt, setJwt }}>{children}</UserContext.Provider>
 }
