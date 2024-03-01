229d8b848b4217e375689349a12bed6ddb17db59
Mehmet Alperen Yedik <121613095+Alperen7K@users.noreply.github.com>
2024-03-01 21:00:04 +0300

    İletişim komponentini öztekniğe uyarladım

--- src/Common/data/ContactData.tsx
+++ src/Common/data/ContactData.tsx
@@ -3,28 +3,28 @@ export const contactdetails = [
     id: 1,
     color: 'primary',
     icon: 'bi bi-geo-alt-fill',
-    title: 'Our Main Office',
-    describe: 'Brusneva Ul., bld. 8, appt. 34, Stavropol, Russia'
+    title: 'Şirket Konumu',
+    describe: 'Serhat, 1471. Sk. No: 2, 06378 İvedik Osb/Yenimahalle/Ankara'
   },
   {
     id: 2,
     color: 'secondary',
     icon: 'bi bi-telephone-fill',
-    title: 'Phone Number',
-    describe: '+(234) 12345 67890<br /> 888-0123-4567 (Toll free)'
+    title: 'Telefon Numarası',
+    describe: '+90 312 395 25 78'
   },
   {
     id: 3,
     color: 'success',
-    icon: 'bi bi-newspaper',
-    title: 'Fax',
-    describe: '1-234-567-8900'
+    icon: 'bi bi-envelope-fill',
+    title: 'Email',
+    describe: 'bilgi@ozteknikrekor.com.tr'
   },
   {
     id: 4,
     color: 'danger',
-    icon: 'bi bi-envelope-fill',
-    title: 'Email',
-    describe: 'support@themesbrand.com'
+    icon: 'bi bi-instagram',
+    title: 'Instagram',
+    describe: '@rekorozteknik'
   }
 ]
