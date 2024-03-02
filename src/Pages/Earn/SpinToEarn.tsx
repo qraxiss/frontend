5d160dc523dbe69403559c0c652ba10cf046da76
qraxiss <qraxiss@gmail.com>
2024-03-02 19:01:01 +0300

    empty commit kanka

--- src/Pages/Earn/SpinToEarn.tsx
+++ src/Pages/Earn/SpinToEarn.tsx
@@ -1,7 +1,7 @@
 export default function TaskRewards() {
     return (
         <section className="section pb-0">
-            <div className="task-rewards">
+            <div className="spin-to-earn">
                 <div className="top-container">
                     <h1>Spin To Earn</h1>
                 </div>
