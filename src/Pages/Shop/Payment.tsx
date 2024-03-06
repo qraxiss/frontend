13461d3d2464336462ef77a52dd0d22c6bd9cce0
qraxiss <qraxiss@gmail.com>
2024-03-06 10:12:44 +0300

    added connecbutton to payment

--- src/Pages/Shop/Payment.tsx
+++ src/Pages/Shop/Payment.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { Card, Col, Container, Row, Tab, Nav } from 'react-bootstrap'
 import { Shoporder } from 'Components/ShopTopBar'
 
+import { ConnectButton } from '@rainbow-me/rainbowkit'
+
 const Payment = () => {
     document.title = 'payment | Toner - React FrontEnd'
     return (
@@ -25,31 +27,7 @@ const Payment = () => {
                                                 <i className="ri-paypal-fill align-bottom"></i>
                                             </span>
                                             <span className="d-none d-sm-block">
-                                                <i className="ri-paypal-fill align-bottom pe-2"></i> Paypal
-                                            </span>
-                                        </Nav.Link>
-                                    </Nav.Item>
-
-                                    <Nav.Item as="li">
-                                        <Nav.Link eventKey="credit" className="py-3">
-                                            <span className="d-block d-sm-none">
-                                                <i className="ri-bank-card-fill align-bottom"></i>
-                                            </span>
-                                            <span className="d-none d-sm-block">
-                                                {' '}
-                                                <i className="ri-bank-card-fill align-bottom pe-2"></i> Credit / Debit Card
-                                            </span>
-                                        </Nav.Link>
-                                    </Nav.Item>
-
-                                    <Nav.Item as="li">
-                                        <Nav.Link eventKey="cash" className="py-3">
-                                            <span className="d-block d-sm-none">
-                                                <i className="ri-money-dollar-box-fill align-bottom"></i>
-                                            </span>
-                                            <span className="d-none d-sm-block">
-                                                {' '}
-                                                <i className="ri-money-dollar-box-fill align-bottom pe-2"></i> Cash on Delivery
+                                                <i className="ri-paypal-fill align-bottom pe-2"></i> Crypto
                                             </span>
                                         </Nav.Link>
                                     </Nav.Item>
@@ -80,104 +58,10 @@ const Payment = () => {
                                                         </label>
                                                         <input type="email" className="form-control" id="email" placeholder="Enter Email Address" />
                                                     </Col>
-
-                                                    <Col md={12}>
-                                                        <label htmlFor="actype" className="form-label">
-                                                            Select your paypal account type
-                                                        </label>
-                                                        <div className="d-flex gap-4 mt-1">
-                                                            <div className="form-check">
-                                                                <input className="form-check-input" type="radio" name="radio1" defaultChecked />
-                                                                <label htmlFor="radio1" className="form-check-label">
-                                                                    Domestic
-                                                                </label>
-                                                            </div>
-                                                            <div className="form-check">
-                                                                <input className="form-check-input" type="radio" name="radio2" />
-                                                                <label htmlFor="radio2" className="form-check-label">
-                                                                    International
-                                                                </label>
-                                                            </div>
-                                                        </div>
-                                                    </Col>
                                                 </Row>
 
                                                 <div className="hstack gap-2 justify-content-end pt-4">
-                                                    <button type="button" className="btn btn-hover btn-primary">
-                                                        <i className="ri-paypal-fill align-bottom align-bottom pe-2"></i> Log into my Paypal
-                                                    </button>
-                                                </div>
-                                            </Card.Body>
-                                        </Card>
-                                    </Tab.Pane>
-
-                                    <Tab.Pane eventKey="credit">
-                                        <Card>
-                                            <Card.Body>
-                                                <Row className="gy-3">
-                                                    <Col md={12}>
-                                                        <label htmlFor="cc-name" className="form-label">
-                                                            Name on card
-                                                        </label>
-                                                        <input type="text" className="form-control" id="cc-name" placeholder="Enter name" />
-                                                        <small className="text-muted">Full name as displayed on card</small>
-                                                    </Col>
-
-                                                    <Col md={6}>
-                                                        <label htmlFor="cc-number" className="form-label">
-                                                            Credit card number
-                                                        </label>
-                                                        <input
-                                                            type="text"
-                                                            className="form-control"
-                                                            id="cc-number"
-                                                            placeholder="xxxx xxxx xxxx xxxx"
-                                                        />
-                                                    </Col>
-
-                                                    <Col md={3}>
-                                                        <label htmlFor="cc-expiration" className="form-label">
-                                                            Expiration
-                                                        </label>
-                                                        <input type="text" className="form-control" id="cc-expiration" placeholder="MM/YY" />
-                                                    </Col>
-
-                                                    <Col md={3}>
-                                                        <label htmlFor="cc-cvv" className="form-label">
-                                                            CVV
-                                                        </label>
-                                                        <input type="text" className="form-control" id="cc-cvv" placeholder="xxx" />
-                                                    </Col>
-                                                </Row>
-
-                                                <div className="hstack gap-2 justify-content-end pt-4">
-                                                    <button type="button" className="btn btn-hover w-md btn-primary">
-                                                        Pay<i className="ri-logout-box-r-line align-bottom ms-2"></i>
-                                                    </button>
-                                                </div>
-                                            </Card.Body>
-                                        </Card>
-                                    </Tab.Pane>
-
-                                    <Tab.Pane eventKey="cash">
-                                        <Card>
-                                            <Card.Body>
-                                                <div className="text-center py-3">
-                                                    <div className="avatar-md mx-auto mb-4">
-                                                        <div className="avatar-title bg-primary-subtle text-primary rounded-circle display-6">
-                                                            <i className="bi bi-cash"></i>
-                                                        </div>
-                                                    </div>
-                                                    <h5 className="fs-16 mb-3">Cash on Delivery</h5>
-                                                    <p className="text-muted mt-3 mb-0 w-75 mx-auto">
-                                                        Integer vulputate metus eget purus maximus porttitor. Maecenas ut porta justo. Donec finibus
-                                                        nec nibh ut urna viverra semper.
-                                                    </p>
-                                                </div>
-                                                <div className="hstack gap-2 justify-content-end pt-3">
-                                                    <button type="button" className="btn btn-hover w-md btn-primary">
-                                                        Continue<i className="ri-logout-box-r-line align-bottom ms-2"></i>
-                                                    </button>
+                                                    <ConnectButton/>
                                                 </div>
                                             </Card.Body>
                                         </Card>
