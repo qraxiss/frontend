a6e6b183b870a24d81c8f2e016048cd922a3a833
qraxiss <qraxiss@gmail.com>
2024-03-20 20:59:19 +0300

    sample datas inited

--- src/context/earn/index.tsx
+++ src/context/earn/index.tsx
@@ -1,6 +1,13 @@
 import { createContext, useContext, useEffect, useState } from 'react'
 
-import { xp as xpGQL, addXp as addXpGQL, loginData as loginDataGQL, spinData as spinDataGQL, spin as spinGQL, loginStreak as loginStreakGQL } from '../../lib/common-queries'
+import {
+    xp as xpGQL,
+    addXp as addXpGQL,
+    loginData as loginDataGQL,
+    spinData as spinDataGQL,
+    spin as spinGQL,
+    loginStreak as loginStreakGQL
+} from '../../lib/common-queries'
 import { useMutation, useQuery } from 'lib/query-wrapper'
 
 import type { EarnContextType } from './types'
@@ -15,23 +22,24 @@ export const EarnContext = createContext<EarnContextType>({
     addXpRES: {},
     addXp: 0,
 
-
-    spinGQL:{},
-    spinRES:{},
-    spin:{},
+    spinGQL: {},
+    spinRES: {},
+    spin: 0,
 
     spinDataGQL: {},
     spinDataRES: {},
-    spinData: {},
+    spinData: 0,
 
-    loginStreakGQL:{},
-    loginStreakRES:{},
-    loginStreak:{},
+    loginStreakGQL: {},
+    loginStreakRES: {},
+    loginStreak: 0,
 
     loginDataGQL: {},
-    loginDataRES:{},
-    loginData: {},
-
+    loginDataRES: {},
+    loginData: {
+        lastLogin: 0,
+        loginCount: 0
+    }
 })
 
 export function useEarn() {
@@ -58,7 +66,6 @@ export function EarnProvider({ children }: { children: any }) {
         }
     }, [spinRES.loading])
 
-
     //////
     let spinDataRES = useQuery(spinDataGQL)
     let [spinData, setSpinData] = useState(0)
@@ -79,7 +86,6 @@ export function EarnProvider({ children }: { children: any }) {
         }
     }, [loginStreakRES.loading])
 
-
     //////
     let loginDataRES = useQuery(loginDataGQL)
     let [loginData, setLoginData] = useState(0)
@@ -89,7 +95,6 @@ export function EarnProvider({ children }: { children: any }) {
             setLoginData(loginDataRES.data)
         }
     }, [loginDataRES.loading])
-
 
     //////
     let addXpRES = useMutation(addXpGQL)
