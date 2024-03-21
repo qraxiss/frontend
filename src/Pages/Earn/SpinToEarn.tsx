828fbf59b2f1e62374c811800dc57795f37a25b1
qraxiss <qraxiss@gmail.com>
2024-03-21 08:18:37 +0300

    when not spinned cant claim

--- src/Pages/Earn/SpinToEarn.tsx
+++ src/Pages/Earn/SpinToEarn.tsx
@@ -4,13 +4,17 @@ import wheel from '../../assets/images/earn/wheel.png'
 
 // @ts-ignore
 import WheelComponent from 'react-wheel-of-prizes'
+import { useState } from 'react'
+import { useEarn } from 'context/earn'
 
 export default function SpinToEarn() {
     const segments = ['10 XP', '20 XP', '30 XP', '40 XP', '50 XP', '60 XP']
     const segColors = ['#13c5f0', '#f556b7', '#13c5f0', '#f556b7', '#13c5f0', '#f556b7']
-    const onFinished = (winner: any) => {
-        console.log(winner)
-    }
+
+    const [prize, setPrize] = useState<string | undefined>(undefined)
+    const [claimAvailable, setClaimAvailable] = useState<boolean>(false)
+
+    let { spinRES } = useEarn()
 
     return (
         <section className="section pb-0">
@@ -20,7 +24,22 @@ export default function SpinToEarn() {
                         <h1>Spin To Earn</h1>
                     </div>
                     <div className="claim">
-                        <Button className="btn btn btn-secondary">Spin</Button>
+                        <Button
+                            disabled={!claimAvailable}
+                            className="btn btn btn-secondary"
+                            onClick={() => {
+                                if (prize) {
+                                    spinRES.fn({
+                                        variables: {
+                                            point: Number(prize.replace(' XP', ''))
+                                        }
+                                    })
+                                    setClaimAvailable(false)
+                                }
+                            }}
+                        >
+                            Claim
+                        </Button>
                         <p>Spin the wheel and win the prize.</p>
                     </div>
 
@@ -33,7 +52,10 @@ export default function SpinToEarn() {
                             <WheelComponent
                                 segments={segments}
                                 segColors={segColors}
-                                onFinished={(winner: any) => onFinished(winner)}
+                                onFinished={(winner: any) => {
+                                    setPrize(winner)
+                                    setClaimAvailable(true)
+                                }}
                                 primaryColor="white"
                                 contrastColor="#faca30"
                                 isOnlyOnce={false}
